e03e7e39255d0c5af7e02830aab092ed4a7811c9
Heru Azy <heruazyp@gmail.com>
2024-01-22 11:21:51 +0700

    Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['*.html'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				dark: '#0A0908',
+				light: '#F2F4F3',
+				primary: '#5E503F',
+				secondary: '#A9927D',
+				tertiary: '#22333B'
+			},
+
+			fontFamily: {
+				inter: ['Inter']
+			}
+		}
 	},
 	plugins: []
 }
