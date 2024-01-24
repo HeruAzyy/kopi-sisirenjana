e8c9c38e29c91d858285d7ee8ade6c7b544429f4
Heru Azy <heruazyp@gmail.com>
2024-01-24 10:39:29 +0700

    Add: hero section

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,4 +10,6 @@ document.addEventListener('click', function (e) {
 	if (!hamburger.contains(e.target) && !navbar.contains(e.target)) {
 		navbar.classList.remove('active')
 	}
+
+	e.preventDefault()
 })
