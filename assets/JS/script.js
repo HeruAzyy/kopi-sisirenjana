bf6f9ee7c28c0e026e0c9e3eb99a18ca58ac7188
Heru Azy <heruazyp@gmail.com>
2024-01-24 12:16:09 +0700

    Add: about section

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,8 @@
 const navbar = document.getElementById('navbar-nav')
 
-document.getElementById('hamburger-menu').onclick = () => {
+document.getElementById('hamburger-menu').onclick = (e) => {
 	navbar.classList.toggle('active')
+	e.preventDefault()
 }
 
 const hamburger = document.getElementById('hamburger-menu')
@@ -10,6 +11,4 @@ document.addEventListener('click', function (e) {
 	if (!hamburger.contains(e.target) && !navbar.contains(e.target)) {
 		navbar.classList.remove('active')
 	}
-
-	e.preventDefault()
 })
