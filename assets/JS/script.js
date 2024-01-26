46db273bf642fb2b27ad49ec4f7bdb67c12b0dc7
Heru Azy <heruazyp@gmail.com>
2024-01-26 15:19:39 +0700

    Add: active search form

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,10 +5,24 @@ document.getElementById('hamburger-menu').onclick = (e) => {
 	e.preventDefault()
 }
 
+const searchForm = document.getElementById('search-form')
+const searchBox = document.getElementById('search-box')
+
+document.getElementById('search-btn').onclick = (e) => {
+	searchForm.classList.toggle('search-active')
+	searchBox.focus()
+	e.preventDefault()
+}
+
 const hamburger = document.getElementById('hamburger-menu')
+const searchBtn = document.getElementById('search-btn')
 
 document.addEventListener('click', function (e) {
 	if (!hamburger.contains(e.target) && !navbar.contains(e.target)) {
 		navbar.classList.remove('active')
+	}
+
+	if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+		searchForm.classList.remove('search-active')
 	}
 })
